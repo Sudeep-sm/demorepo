a2ec699c79a024fe5853859ddba7c0def769e965
Sudeep-sm <noobadum00@gmail.com>
2025-04-23 14:50:36 +0530

    save

--- Demo.java
+++ Demo.java
@@ -1,5 +1,14 @@
 package dev;
 
 public class Demo {
+    public static void main(String[] args){
+        int a=10;
+        int b=5; 
+        System.out.println("The addition result is:"+(a+b));
+        System.out.println("The subtraction result is:"+(a-b));
+        System.out.println("The multiplication result is:"+(a*b));
+        System.out.println("The division result is:"+(a/b));
+        
+    }
 
 }
