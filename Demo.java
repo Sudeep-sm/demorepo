262fffbc5f96d8e186c46df27ebb469c71cedc9c
Sudeep-sm <noobadum00@gmail.com>
2025-04-23 17:20:23 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -6,8 +6,7 @@ public class Demo {
         int b=5; 
         System.out.println("The addition result is:"+(a+b));
         System.out.println("The subtraction result is:"+(a-b));
-        System.out.println("The multiplication result is:"+(a*b));
-        System.out.println("The division result is:"+(a/b));
+
         
     
 
